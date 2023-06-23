a6bcaf22f580533de30f7087e163db46a466bb9c
kajal biswas <pikb2015@gmail.com>
2023-06-23 12:29:50 +0530

    appjs

--- shopapp/src/App.js
+++ shopapp/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Header/>
       <Routes>
     <Route exact path='/' element={<Homescreen/>}></Route>
-    <Route path="/product/:id" element={<Productscreen/>}></Route>
+    <Route path='/product/:id' element={<Productscreen/>}></Route>
     </Routes>
       <Footer/>
       </Router>
