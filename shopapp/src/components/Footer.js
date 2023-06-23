5db406b187bcf835d3682170e65892b041523346
kajal biswas <pikb2015@gmail.com>
2023-06-23 10:58:52 +0530

    ui

--- shopapp/src/components/Footer.js
+++ shopapp/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bottom-1 p-2 w-full text-center'>
-      <h1 className=''>Copyright RoyalJute 2023</h1>
+      <h1 className=' font-bold text-sm'>Copyright RoyalJute 2023</h1>
       </footer>
   )
 }
