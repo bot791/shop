5773bee5c92556a9d4880cc0fe0d54c12e881679
kajal biswas <pikb2015@gmail.com>
2023-06-23 12:29:30 +0530

    useparams

--- shopapp/src/screens/Productscreen.js
+++ shopapp/src/screens/Productscreen.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import products from '../products'
+import { useParams } from 'react-router-dom'
 
-const Productscreen = ({match}) => {
-    const product=products.find((p)=>p._id === match.params.id
+const Productscreen = () => {
+    const {id}=useParams();
+    const product=products.find((p)=>p._id === (id)
     )
   return (
     <div className=' w-[60%] h-[80%] bg-zinc-200 rounded block m-auto mt-2 items-center'>
-<div>{product.name}</div>    </div>
+<img src={product.image} alt='' className=''/>
+    </div>
   )
 }
 
