d99253564dab901bb9b85e066bfd0d5e766a5489
kajal biswas <pikb2015@gmail.com>
2023-06-23 11:38:03 +0530

    router

--- shopapp/src/App.js
+++ shopapp/src/App.js
@@ -1,13 +1,18 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Homescreen from "./screens/Homescreen";
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="h-screen ">
+      <Router>
       <Header/>
-     <Homescreen/>
+      <Routes>
+    <Route exact path='/' element={<Homescreen/>}></Route>
+    </Routes>
       <Footer/>
+      </Router>
     </div>
   );
 }
--- shopapp/src/components/Product.js
+++ shopapp/src/components/Product.js
@@ -6,7 +6,6 @@ const Product = ({product}) => {
         <img src={product.image} alt=''/>
 
         <h1 className=' font-bold text-lg  p-2   text-center text-emerald-500'>{product.name}</h1>
-        <p className=' font-semibold text-sm p-2 text-center text-neutral-700'>{product.description}</p>
         <p className=' font-bold font-mono       text-center text-gray-900'>Rs.{product.price}</p>
         <p className=' font-mono font-semibold text-center text-gray-700'>In Stock {product.countInStock}</p>
     </div>
