1752d3748e996b9b935f4b4656c41a41706392e4
kajal biswas <pikb2015@gmail.com>
2023-06-23 12:13:54 +0530

    products

--- shopapp/src/App.js
+++ shopapp/src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Homescreen from "./screens/Homescreen";
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Productscreen from "./screens/Productscreen";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Routes>
     <Route exact path='/' element={<Homescreen/>}></Route>
+    <Route path="/product/:id" element={<Productscreen/>}></Route>
     </Routes>
       <Footer/>
       </Router>
--- shopapp/src/screens/Productscreen.js
+++ shopapp/src/screens/Productscreen.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import products from '../products'
 
-const Productscreen = () => {
+const Productscreen = ({match}) => {
+    const product=products.find((p)=>p._id === match.params.id
+    )
   return (
-    <div>Productscreen</div>
+    <div className=' w-[60%] h-[80%] bg-zinc-200 rounded block m-auto mt-2 items-center'>
+<div>{product.name}</div>    </div>
   )
 }
 
