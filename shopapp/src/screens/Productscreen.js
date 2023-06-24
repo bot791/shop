0cb67743c5342a35649127049b1995c85dd415a8
kajal biswas <pikb2015@gmail.com>
2023-06-24 18:58:45 +0530

    productscreen

--- shopapp/src/screens/Productscreen.js
+++ shopapp/src/screens/Productscreen.js
@@ -14,7 +14,7 @@ const Productscreen = () => {
   <button className=' text-zinc-100 ring-2 bg-emerald-900 hover:bg-emerald-500 ring-white text-center m-2 px-4 py-1 rounded font-bold'>ADD TO CART</button>
 </div>
 <div className=' flex flex-col'>
-  <p className=' font-bold font-mono m-10 text-xl text-gray-800'>{product.name}</p>
+  <p className=' font-bold font-mono m-10 text-2xl text-gray-800'>{product.name}</p>
   <p className=' font-bold text-lg px-5'>Brand: {product.brand}</p>
   <p className=' font-semibold m-5'>{product.description}</p>
   <p className=' px-5 font-medium font-serif text-lg text-zinc-700'>In stock: {product.countInStock}</p>
