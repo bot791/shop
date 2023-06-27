9b4501804efa9e0be4b07368b58c452608172418
kajal biswas <pikb2015@gmail.com>
2023-06-27 18:32:26 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
+const products = require('./data/products');
+const { useParams } = require('react-router-dom');
 
 
+app.get('/',(req,res)=>{
+res.send('api is running')
+})
+app.get('/api/products',(req,res)=>{
+res.json(products)
+})
+
+app.get('/api/products/:id',(req,res)=>{
+    const {id}=useParams();
+const product = products.find((p)=>{
+p._id === id
+})
+res.json(product)
+}
+)
 
 app.listen(5000,console.log("server running on port 5000"))
 
