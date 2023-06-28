862908a329ed7f4e7e92fa13b877280b8741938f
kajal biswas <pikb2015@gmail.com>
2023-06-28 12:57:42 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,7 @@ app.get('/api/products',(req,res)=>{
 res.json(products)
 })
 
-app.get('/api/products/:id',(req,res)=>{
-    const {id}=useParams();
-const product = products.find(p=>
-p._id === id
-)
-res.json(product)
-}
-)
+
 
 app.listen(5000,console.log("server running on port 5000"))
 
