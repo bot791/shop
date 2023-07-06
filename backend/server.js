4193dc5a48064d9886a4b0bb8b53df85af5aa556
kajal biswas <pikb2015@gmail.com>
2023-07-06 18:16:39 +0530

    import syntax

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const app = express();
-const products = require("./data/products");
-const dotenv = require('dotenv')
+import express from "express"
+import products from "./data/products.js"
+import dotenv from 'dotenv'
 
 dotenv.config()
+
+const app = express();
+
 
 app.get("/", (req, res) => {
   res.send("api started...");
