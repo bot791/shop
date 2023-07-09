28ccbcd6d06894cd8bfbf42fbf3cfda8b052ff74
kajal biswas <pikb2015@gmail.com>
2023-07-09 19:16:56 +0530

    footerstyles

--- shopapp/src/components/Footer.js
+++ shopapp/src/components/Footer.js
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bottom-1 p-2 w-full text-center">
-      <h1 className=" font-bold text-sm">Copyright RoyalJute 2023</h1>
+      <h1 className=" font-bold text-sm text-emerald-950 font-mono">Copyright @RoyalJute 2023</h1>
+      <h1 className=" font-mono font-thin text-xs ">A quality Jute product maker in India</h1>
     </footer>
   );
 };
