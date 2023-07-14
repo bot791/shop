8fd49e66f8b166a2bfc3339e7755b9d00e3d4e15
kajal biswas <pikb2015@gmail.com>
2023-07-14 13:09:55 +0530

    ui changes

--- shopapp/src/components/Footer.js
+++ shopapp/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bottom-1 p-2 w-full text-center">
-      <h1 className=" font-bold text-sm text-emerald-950 font-mono">Copyright @RoyalJute 2023</h1>
+      <h1 className=" font-bold text-sm text-emerald-950 font-mono">Copyright @Royal Brothers 2023</h1>
       <h1 className=" font-mono font-thin text-xs ">A quality Jute product maker in India</h1>
     </footer>
   );
