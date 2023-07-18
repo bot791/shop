33b614e32c117e221ab8e0ee6e35e6dabff4c8f4
kajal biswas <pikb2015@gmail.com>
2023-07-18 13:03:46 +0530

    ordermodel

---
+++ backend/models/orderModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema({
+    name:{
+        type: mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref: 'User'
+    },
+    orderItems:[{
+       name:{type:String,required:true},
+       qty:{type:Number,required:true},
+       image:{type:String,required:true},
+       price:{type:Number,required:true},
+       product:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'Product'}
+    }],
+    shippingAddress:{
+       address:{type:String,required:true},
+       city:{type:String,required:true},
+       postalCode:{type:Number,required:true},
+       country:{type:String,required:ture}
+    },
+    paymentMethod:{
+       type:String,required:true
+    },
+    
+})
+
+const User = mpngoose.model('User',userSchema)
+
+export default User
