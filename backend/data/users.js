544fcfab0c77387a362c514ea83c6d2da565b790
kajal biswas <pikb2015@gmail.com>
2023-07-26 18:18:48 +0530

    bcrypt users

---
+++ backend/data/users.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name:'admin name',
+        email:'admin@example.com',
+        password: xxxx,
+        isAdmin:true
+    },
+    {
+        name:'name name',
+        email:'adminname@example.com',
+        password: xxxx,
+    },
+    {
+        name:'noob name',
+        email:'adminnoob@example.com',
+        password: xxxx,
+    }
+]
