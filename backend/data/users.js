ba3b3e24a8317e81be7252a1409e89c9b01bd20d
kajal biswas <pikb2015@gmail.com>
2023-07-28 18:31:06 +0530

    seeder

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,17 +4,19 @@ const users = [
     {
         name:'admin name',
         email:'admin@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('12345',10),
         isAdmin:true
     },
     {
         name:'name name',
         email:'adminname@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('12345',10),
     },
     {
         name:'noob name',
         email:'adminnoob@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('12345',10),
     }
 ]
+
+export default users
---
+++ backend/seeder.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import users from './data/Users.js'
+import products from './data/products.js'
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
