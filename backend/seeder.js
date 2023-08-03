d609185d701cc11509327720bf33ec062461b3c8
kajal biswas <pikb2015@gmail.com>
2023-08-03 10:37:24 +0530

    seeder

--- backend/seeder.js
+++ backend/seeder.js
@@ -5,3 +5,41 @@ import products from './data/products.js'
 import User from './models/userModel.js'
 import Product from './models/productModel.js'
 import Order from './models/orderModel.js'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+connectDB()
+
+const importData = async () =>{
+    try{
+await Order.deleteMany()
+await Product.deleteMany()
+await User.deleteMany()
+const createUsers = await User.insertMany(users)
+
+const adminUser = createUsers[0]._id
+
+const sampleProducts = products.map(product => {
+    return{...product,user:adminUser}
+})
+await Product.insertMany(sampleProducts)
+console.log('data imported')
+    }catch(error){
+console.error({error})
+process.exit(1)
+    }
+}
+
+const destroyData = async () =>{
+    try{
+await Order.deleteMany()
+await Product.deleteMany()
+await User.deleteMany()
+
+console.log('data destroyed')
+    }catch(error){
+console.error({error})
+process.exit(1)
+    }
+}
