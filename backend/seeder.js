cb93ea75a470184cb7fe269266c6e820fe0fa008
kajal biswas <pikb2015@gmail.com>
2023-08-21 17:49:25 +0530

    seeder

--- backend/seeder.js
+++ backend/seeder.js
@@ -16,9 +16,9 @@ const importData = async () =>{
 await Order.deleteMany()
 await Product.deleteMany()
 await User.deleteMany()
-const createUsers = await User.insertMany(users)
+const createdUsers = await User.insertMany(users)
 
-const adminUser = createUsers[0]._id
+const adminUser = createdUsers[0]._id
 
 const sampleProducts = products.map(product => {
     return{...product,user:adminUser}
