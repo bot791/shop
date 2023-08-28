655fcd1e519125a6694d1e84353b0f6234ffa967
kajal biswas <pikb2015@gmail.com>
2023-08-28 16:13:16 +0530

    products js

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,6 @@
 const products = [
-    {
-      _id:'1',
+    {      _id:'1',
+
       name: 'Airpods Wireless Bluetooth Headphones',
       image: '/images/airpods.jpg',
       description:
@@ -14,6 +14,7 @@ const products = [
     },
     {
       _id:'2',
+
       name: 'iPhone 11 Pro 256GB Memory',
       image: '/images/phone.jpg',
       description:
@@ -27,6 +28,7 @@ const products = [
     },
     {
       _id:'3',
+
       name: 'Cannon EOS 80D DSLR Camera',
       image: '/images/camera.jpg',
       description:
@@ -40,6 +42,7 @@ const products = [
     },
     {
       _id:'4',
+
       name: 'Sony Playstation 4 Pro White Version',
       image: '/images/playstation.jpg',
       description:
@@ -53,6 +56,7 @@ const products = [
     },
     {
       _id:'5',
+
       name: 'Logitech G-Series Gaming Mouse',
       image: '/images/mouse.jpg',
       description:
@@ -66,6 +70,7 @@ const products = [
     },
     {
       _id:'6',
+
       name: 'Amazon Echo Dot 3rd Generation',
       image: '/images/alexa.jpg',
       description:
