a19eeb376c697ecd0a3901fd4030e542c409eda2
[dipleo-dev] <dipleo.dev@gmail.com>
2024-01-20 01:03:50 +0530

    modification

--- shopapp/src/components/LandingPage.js
+++ shopapp/src/components/LandingPage.js
@@ -14,7 +14,7 @@ const LandingPage = () => {
           </div>
           <div className=" bg-gradient-to-tr from-gray-200 to-gray-200 rounded-xl p-2">
             <p className="text-6xl sm:text-7xl md:text-9xl font-extrabold font-Phily text-center text-teal-800">
-              We Make Products From Jute
+              We Offer you the best Discount & Service
             </p>
           </div>
         </div>
