835bf77bb3e1ea90e56f162a3f194537cd52413c
kajal biswas <pikb2015@gmail.com>
2024-01-27 19:30:45 +0530

    resisteruser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,10 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
   if (user && (await user.matchpassword(password))) {
     res.json({
+      _id: user._id,
+      name: user.name,
       email: user.email,
+      isAdmin: user.isAdmin,
       token: generateToken(user._id),
     });
   } else {
@@ -15,9 +18,48 @@ const authUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid email or password");
   }
 });
+//resister user
 
-const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("success");
+const resisterUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.sendStatus(400);
+    throw new Error("user already exists!");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid user data!");
+  }
 });
 
-export { authUser, getUserProfile };
+//user profile
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("user not found");
+  }
+});
+
+export { authUser, getUserProfile, resisterUser };
